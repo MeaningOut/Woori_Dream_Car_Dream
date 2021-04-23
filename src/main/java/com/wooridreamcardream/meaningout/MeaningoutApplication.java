2b5f25dc07d6d9faf1e2ccaa04a0b8012d28f48b
nayoon-kim <belloyv@gmail.com>
2021-04-23 11:44:37 +0900

    start project

---
+++ src/main/java/com/wooridreamcardream/meaningout/MeaningoutApplication.java
@@ -0,0 +1,13 @@
+package com.wooridreamcardream.meaningout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeaningoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeaningoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wooridreamcardream/meaningout/MeaningoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wooridreamcardream.meaningout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeaningoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

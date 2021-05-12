1ac8251fc3c6155df03de8d0132bc65b760c95a5
김나윤 <belloyv@gmail.com>
2021-05-12 23:23:49 +0900

    test homeController

---
+++ src/test/java/com/wooridreamcardream/meaningout/controller/HomeControllerTest.java
@@ -0,0 +1,43 @@
+package com.wooridreamcardream.meaningout.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class HomeControllerTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void index_페이지_로딩() {
+        //when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        //then
+        assertThat(body).contains("우리은행에서 신차를 사는 새로운 방법");
+    }
+
+    @Test
+    public void home_페이지_로딩() {
+        //when
+        String body = this.restTemplate.getForObject("/home", String.class);
+
+        //then
+        assertThat(body).contains("연소득과 대출한도는 꼭 선택해주세요!");
+    }
+
+    @Test
+    public void sub_페이지_로딩() {
+        //when
+        String body = this.restTemplate.getForObject("/sub", String.class);
+
+        //then
+        assertThat(body).contains("어떤 소비신념을 가지셨나요?");
+    }
+}

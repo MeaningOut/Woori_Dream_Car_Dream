6e9cc9532c059aa4c1f38765c56f7d6ce551606b
nayoon-kim <belloyv@gmail.com>
2025-08-02 12:08:46 +0900

    refactor: remove unused import

--- src/main/java/com/wooridreamcardream/meaningout/repository/CarRepository.java
+++ src/main/java/com/wooridreamcardream/meaningout/repository/CarRepository.java
@@ -3,7 +3,6 @@ package com.wooridreamcardream.meaningout.repository;
 import com.wooridreamcardream.meaningout.domain.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 

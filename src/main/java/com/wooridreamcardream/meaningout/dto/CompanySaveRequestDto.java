8e70e7265bcdf2e80b73fc15e3eb927afdc679bd
nayoon-kim <belloyv@gmail.com>
2025-08-02 12:05:49 +0900

    refactor(dto): remove blank

--- src/main/java/com/wooridreamcardream/meaningout/dto/CompanySaveRequestDto.java
+++ src/main/java/com/wooridreamcardream/meaningout/dto/CompanySaveRequestDto.java
@@ -3,7 +3,6 @@ package com.wooridreamcardream.meaningout.dto;
 import com.wooridreamcardream.meaningout.domain.Company;
 
 public record CompanySaveRequestDto(String companyName) {
-
     public Company toEntity() {
         return Company.builder()
                 .name(companyName)

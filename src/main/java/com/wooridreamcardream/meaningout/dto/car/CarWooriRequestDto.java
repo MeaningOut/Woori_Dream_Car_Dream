b2aa5af7395f2411243bf2fd268b61906f85b77e
nayoon-kim <belloyv@gmail.com>
2025-08-02 11:58:30 +0900

    refactor(dto): make class -> record

--- src/main/java/com/wooridreamcardream/meaningout/dto/car/CarWooriRequestDto.java
+++ src/main/java/com/wooridreamcardream/meaningout/dto/car/CarWooriRequestDto.java
@@ -1,24 +1,7 @@
 package com.wooridreamcardream.meaningout.dto.car;
 
 import lombok.Builder;
-import lombok.Getter;
 
-@Getter
-public class CarWooriRequestDto {
-    private Long carId;
-    private RequestDataBody dataBody;
-
-    @Builder
-    public CarWooriRequestDto(Long carId, RequestDataBody dataBody) {
-        this.carId = carId;
-        this.dataBody = dataBody;
-    }
-
-    @Override
-    public String toString() {
-        return "CarWooriRequestDto{" +
-                "carId=" + String.valueOf(carId) +
-                ", dataBody=" + dataBody +
-                '}';
-    }
+@Builder
+public record CarWooriRequestDto(Long carId, RequestDataBody dataBody) {
 }
